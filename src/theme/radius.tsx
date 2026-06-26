fffb86d713969af88cba17f84f3e4c68703981ae
Mayor Creator <paulaboagye@outlook.com>
2026-06-26 11:37:09 -0400

    add theme to handle radius values

---
+++ src/theme/radius.tsx
@@ -0,0 +1,14 @@
+export const scope = {
+	radius: {
+		radius0: 0,
+		radius4: 4,
+		radius6: 6,
+		radius8: 8,
+		radius10: 10,
+		radius12: 12,
+		radius16: 16,
+		radius20: 20,
+		radius24: 24,
+		radiusFull: 999,
+	},
+};
